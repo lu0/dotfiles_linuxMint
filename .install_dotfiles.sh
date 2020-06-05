973a099c025127f8cff08661136dfe7e4aa94924
lu0alv <lucero.alvaradorz@gmail.com>
2020-06-05 12:56:36 -0500

    vscode installation

--- .install_dotfiles.sh
+++ .install_dotfiles.sh
@@ -83,6 +83,14 @@ dconf load / < ~/.dconf.conf
 chmod +775 ~/dotfiles_linuxMint/config/export_dconf.sh
 
 # VS code --------------------------------------------------------------------------------------
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
+
+
 rm -rf ~/.config/Code/User/settings.json
 ln -s ~/dotfiles_linuxMint/config/vscode/settings_FHD.json ~/.config/Code/User/settings.json
 # ln -s ~/dotfiles_linuxMint/config/vscode/settings_HD.json ~/.config/Code/User/settings.json
