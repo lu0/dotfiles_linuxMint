cc9567a0b33e19bbb189ec699ee397460e46c25f
lu0alv <lucero.alvaradorz@gmail.com>
2020-06-08 14:39:03 -0500

    dconf files by category

--- dconf-files/dconf-load.sh
+++ dconf-files/dconf-load.sh
@@ -2,3 +2,5 @@
 
 dconf load /org/gnome/terminal/ < dconf-files/gnome-terminal.conf
 dconf load /org/cinnamon/ < ~/dotfiles_linuxMint/dconf-files/system-sounds.conf
+dconf load / < ~/dotfiles_linuxMint/dconf-files/miscellaneous.conf
+dconf load / < ~/dotfiles_linuxMint/dconf-files/desktop-keybindings.conf
