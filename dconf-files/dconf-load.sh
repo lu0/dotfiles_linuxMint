306013ea33ca61998b2e9ca9071284baeb5618b5
lu0 <lucero.alvaradorz@gmail.com>
2020-06-09 17:59:07 -0500

    nemo config files

--- dconf-files/dconf-load.sh
+++ dconf-files/dconf-load.sh
@@ -5,4 +5,5 @@ dconf load /org/gnome/terminal/ < gnome-terminal.conf
 dconf load /org/cinnamon/ < system-sounds.conf
 dconf load / < miscellaneous.conf
 dconf load / < desktop-keybindings.conf
+dconf load / < nemo-fileman.conf
 cd ..
