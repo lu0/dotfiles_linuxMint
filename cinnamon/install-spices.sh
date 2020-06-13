2a697f8965d4b7281471399cfd339934bcf82d62
lu0 <lucero.alvaradorz@gmail.com>
2020-06-12 20:44:32 -0500

    Clean theme files RedVariation

--- cinnamon/install-spices.sh
+++ cinnamon/install-spices.sh
@@ -7,6 +7,12 @@
 
 RUN_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+sudo rm /usr/share/icons/hicolor/scalable/apps/linuxmint-logo-badge-symbolic.svg
+sudo ln -sr $RUN_DIR/appearance/icons/linuxmint-logo-badge-symbolic.svg /usr/share/icons/hicolor/scalable/apps/
+
+sudo rm /usr/share/icons/hicolor/scalable/apps/cinnamon-symbolic.svg
+sudo ln -sr $RUN_DIR/appearance/icons/cinnamon-symbolic.svg /usr/share/icons/hicolor/scalable/apps/
+
 rm -rf ~/.local/share/cinnamon/applets
 ln -sr $RUN_DIR/spices_files/applets ~/.local/share/cinnamon/applets
 
