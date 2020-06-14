5f4faf315c03c2accc76be414cfa8efc171ffe75
lu0 <lucero.alvaradorz@gmail.com>
2020-06-14 12:46:35 -0500

    left panel only

--- cinnamon/appearance/devilspie2/maximize_wGaps.lua
+++ cinnamon/appearance/devilspie2/maximize_wGaps.lua
@@ -1,7 +1,7 @@
 
-if (get_window_type() ~= "WINDOW_TYPE_DIALOG") then
+if (get_window_type() == "WINDOW_TYPE_NORMAL") then
     set_on_top();
     unmaximize();
     set_opacity(1.0);
-    set_window_geometry(46,46,1864,1024);
+    set_window_geometry(46,10,1864,1060);
 end
