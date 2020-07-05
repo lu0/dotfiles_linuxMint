87c72dcab62ed54c7d63aac2d05f2f08025f115a
lu0 <lucero.alvaradorz@gmail.com>
2020-07-05 02:12:11 -0500

    Papirus Installer for Icon theme and Colors

---
+++ cinnamon/appearance/icons/Papirus/install-papirus.sh
@@ -0,0 +1,12 @@
+# Install Papirus icons
+# As described in 
+# github.com/PapirusDevelopmentTeam/papirus-icon-theme
+
+# Usage:
+# List colors: 
+#   papirus-folders -l
+# Change colors:
+#   papirus-folders --color black --theme Papirus-Dark 
+
+wget -qO- https://git.io/papirus-icon-theme-install | sh
+wget -qO- https://git.io/papirus-folders-install | sh
