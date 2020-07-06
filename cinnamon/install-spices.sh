367971185b2801daeb3c21f3cf077984322d66f6
lu0 <lucero.alvaradorz@gmail.com>
2020-07-06 17:31:19 -0500

    Track applets in submodule

--- cinnamon/install-spices.sh
+++ cinnamon/install-spices.sh
@@ -7,14 +7,14 @@
 
 RUN_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# Cinnamon and Mint icons for 34px panels 
 sudo rm /usr/share/icons/hicolor/scalable/apps/linuxmint-logo-badge-symbolic.svg
-sudo ln -sr $RUN_DIR/appearance/icons/linuxmint-logo-badge-symbolic.svg /usr/share/icons/hicolor/scalable/apps/
-
 sudo rm /usr/share/icons/hicolor/scalable/apps/cinnamon-symbolic.svg
+sudo ln -sr $RUN_DIR/appearance/icons/linuxmint-logo-badge-symbolic.svg /usr/share/icons/hicolor/scalable/apps/
 sudo ln -sr $RUN_DIR/appearance/icons/cinnamon-symbolic.svg /usr/share/icons/hicolor/scalable/apps/
 
 rm -rf ~/.local/share/cinnamon/applets
-ln -sr $RUN_DIR/spices_files/applets ~/.local/share/cinnamon/applets
+ln -sr $RUN_DIR/spices-applets/ ~/.local/share/cinnamon/applets
 
 rm -rf ~/.local/share/cinnamon/extensions
 ln -sr $RUN_DIR/spices_files/extensions ~/.local/share/cinnamon/extensions
