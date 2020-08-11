5619f7e668f4aa34e617d776efa9336d0e0289fd
lu0 <lucero.alvaradorz@gmail.com>
2020-08-11 10:44:31 -0500

    dconfs. screencast format, nemo compact-view

--- dconf-files/dconf-load.sh
+++ dconf-files/dconf-load.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#
+# Wiki
+# https://github.com/linuxmint/Cinnamon/wiki/Backing-up-and-restoring-your-cinnamon-settings-(dconf)
+# 
+
 cd dconf-files
 dconf load / < cinnamon-theme.conf
 dconf load / < desktop-keybindings.conf
