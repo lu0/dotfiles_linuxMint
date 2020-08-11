29f00427841c6d5bc8d1e2c8524411770448adc5
lu0 <lucero.alvaradorz@gmail.com>
2020-08-11 16:06:45 -0500

    move windows with scripts

---
+++ scripts/window-control/move-rl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get x position
+px="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'position:' | cut -d ':' -f 2 | cut -d ',' -f 1)"
+
+# Get y position
+py="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'position:' | cut -d ',' -f 2 | cut -d ' ' -f 1)"
+
+# Get x geometry
+gx="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'geometry:' | cut -d ':' -f 2 | cut -d 'x' -f 1)"
+
+# Get y geometry
+gy="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'geometry:' | cut -d ':' -f 2 | cut -d 'x' -f 2)"
+
+res=$(($px + $py))
+
+wmctrl -r :ACTIVE: -e 0,$((px - 12)),$((py - 12)),$((gx - 10)),$((gy))
