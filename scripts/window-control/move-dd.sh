aa19fda291367c3adbcedc1de879bc43d421445c
lu0 <lucero.alvaradorz@gmail.com>
2020-08-11 22:46:12 -0500

    window scripts: ignore window decorations

--- scripts/window-control/move-dd.sh
+++ scripts/window-control/move-dd.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Get x position
-px="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'position:' | cut -d ':' -f 2 | cut -d ',' -f 1)"
+# 
+# Move window bottom-to-bottom
+# 1920x1080 resolutions
+#
+# author: github.com/lu0
+#
 
-# Get y position
-py="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'position:' | cut -d ',' -f 2 | cut -d ' ' -f 1)"
+# Get absolute positions
+px="$(xwininfo -id $(xdotool getactivewindow) | egrep -i "Absolute upper-left X" | cut -d ':' -f 2)"
+py="$(xwininfo -id $(xdotool getactivewindow) | egrep -i "Absolute upper-left Y" | cut -d ':' -f 2)"
 
-# Get x geometry
-gx="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'geometry:' | cut -d ':' -f 2 | cut -d 'x' -f 1)"
+# Get relative positions
+rx="$(xwininfo -id $(xdotool getactivewindow) | egrep "Relative upper-left X" | cut -d ':' -f 2)"
+ry="$(xwininfo -id $(xdotool getactivewindow) | egrep "Relative upper-left Y" | cut -d ':' -f 2)"
 
-# Get y geometry
-gy="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'geometry:' | cut -d ':' -f 2 | cut -d 'x' -f 2)"
+# Gnome apps have identical absolute and relative positions,
+# other windows need a 2px correction when setting their position
+[[ $px -eq $rx ]] && fix=0 || fix=2
 
-res=$(($px + $py))
+# Get geometries
+gx="$(xwininfo -id $(xdotool getactivewindow) | egrep "Width" | cut -d ':' -f 2)"
+gy="$(xwininfo -id $(xdotool getactivewindow) | egrep "Height" | cut -d ':' -f 2)"
 
-wmctrl -r :ACTIVE: -e 0,$((px - 12)),$((py - 12)),$((gx)),$((gy + 10))
+# Set new position and geometry
+wmctrl -r :ACTIVE: -e 0,$((px - fix)),$((py - fix)),$((gx)),$((gy + 10))
--- scripts/window-control/move-lr.sh
+++ scripts/window-control/move-lr.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Get x position
-px="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'position:' | cut -d ':' -f 2 | cut -d ',' -f 1)"
+# 
+# Move window left-to-left
+# 1920x1080 resolutions
+#
+# author: github.com/lu0
+#
 
-# Get y position
-py="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'position:' | cut -d ',' -f 2 | cut -d ' ' -f 1)"
+# Get absolute positions
+px="$(xwininfo -id $(xdotool getactivewindow) | egrep -i "Absolute upper-left X" | cut -d ':' -f 2)"
+py="$(xwininfo -id $(xdotool getactivewindow) | egrep -i "Absolute upper-left Y" | cut -d ':' -f 2)"
 
-# Get x geometry
-gx="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'geometry:' | cut -d ':' -f 2 | cut -d 'x' -f 1)"
+# Get relative positions
+rx="$(xwininfo -id $(xdotool getactivewindow) | egrep "Relative upper-left X" | cut -d ':' -f 2)"
+ry="$(xwininfo -id $(xdotool getactivewindow) | egrep "Relative upper-left Y" | cut -d ':' -f 2)"
 
-# Get y geometry
-gy="$(xdotool getwindowfocus getwindowgeometry | egrep -i 'geometry:' | cut -d ':' -f 2 | cut -d 'x' -f 2)"
+# Gnome apps have identical absolute and relative positions,
+# other windows need a 2px correction when setting their position
+[[ $px -eq $rx ]] && fix=0 || fix=2
 
-res=$(($px + $py))
+# Get geometries
+gx="$(xwininfo -id $(xdotool getactivewindow) | egrep "Width" | cut -d ':' -f 2)"
+gy="$(xwininfo -id $(xdotool getactivewindow) | egrep "Height" | cut -d ':' -f 2)"
 
-sleep 0.15 && wmctrl -r :ACTIVE: -e 0,$((px - 12 + 10)),$((py - 12)),$((gx - 10)),$((gy))
+# Set new position and geometry
+sleep 0.05 && wmctrl -r :ACTIVE: -e 0,$((px - fix + 10)),$((py - fix)),$((gx - 10)),$((gy))
