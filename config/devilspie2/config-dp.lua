6ca06426a94df37d3907958466597dffa5d2f968
lu0 <lucero.alvaradorz@gmail.com>
2020-08-20 05:05:58 -0500

    devilspie config: comments

--- config/devilspie2/config-dp.lua
+++ config/devilspie2/config-dp.lua
@@ -1,4 +1,9 @@
+----------------------------------------------------------------
+-- My Devilspie2 configuration for 1920x1080 resolutions
+-- github.com/lu0
+--
 -- Use "devilspie2 --debug" to print to stdout
+----------------------------------------------------------------
 debug_print("Window type: " .. get_window_type())
 debug_print("Window Name: " .. get_window_name());
 debug_print("Application name: " .. get_application_name())
@@ -16,54 +21,56 @@ if (get_window_type() == "WINDOW_TYPE_DIALOG") then
     focus();
     if (not string.match(get_class_instance_name(), "gnome")) then
         debug_print("Not a Gnome dialog");
-        set_window_geometry2(265-12, 90-12, 1454, 925);     -- center
+        -- Set default dimensions of dialogs
+        -- Except for gnome apps (they act weird..)
+        set_window_geometry2(265-12, 90-12, 1454, 925);
     end
 end
 
--- Design Workspace
+-- Workspace 2. I use it for graphic design
 if (get_class_instance_name()=="inkscape" or get_class_instance_name()=="krita") then
     set_window_workspace(2);
     change_workspace(2);
     -- Inkscape dialog windows are of type "NORMAL", but aren't name as "Inkscape"
     if (get_class_instance_name()~="krita" and 
         not string.match(get_window_name(), "- Inkscape")) then
-        -- Move floating inkscape dialogs to right 
+        -- Move floating inkscape dialogs to the right 
         set_window_geometry2(1453, 121, 361, 866);
     end
 end
 
--- Default workspace
+-- Workspace 3. Productivity programs I use
 if (get_class_instance_name()=="Visual Studio Code" or
-    get_application_name()=="DesktopEditors" or
-    get_class_instance_name()=="audacity" or
-    get_class_instance_name()=="kdenlive") then
+    get_application_name()=="DesktopEditors") then
     set_window_workspace(3);
     change_workspace(3);
 end
 
--- File explorer
+-- Workspace 4. File explorer
 if (get_application_name()=="nemo") then
     set_window_workspace(4);
     change_workspace(4);
 end
 
--- System workspace
+-- Workspace 5. System workspace
 if (get_application_name()=="cinnamon-settings.py") then
     set_window_geometry(44, 8, 1864, 1060);
     set_window_workspace(5);
     change_workspace(5);
 end
 
--- Gaming workspace
+-- Workspace 7. Gaming workspace
 if (get_class_instance_name()=="Steam") then
     set_window_workspace(7);
     change_workspace(7);
 end
 
--- Media
+-- Workspace 8. Media
 if (get_application_name()=="Spotify" or 
     get_application_name()=="pavucontrol" or
-    get_application_name()=="blueman-manager") then
+    get_application_name()=="blueman-manager" or
+    get_class_instance_name()=="audacity" or
+    get_class_instance_name()=="kdenlive") then
     set_window_workspace(8);
     change_workspace(8);
 end
