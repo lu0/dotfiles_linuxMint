92b2c3c83e76a4d5b5b9ccde22193071b1443a86
lu0 <lucero.alvaradorz@gmail.com>
2020-12-07 23:12:37 -0600

    fix screencast script

--- scripts/screencast-pulse.sh
+++ scripts/screencast-pulse.sh
@@ -3,6 +3,9 @@
 # Record screen and audio with ffmpeg
 # lu0
 
+# list recording devices:
+# pacmd list-sources|awk '/index:/ {print $0}; /name:/ {print $0}; /device\.description/ {print $0}'
+
 if pgrep ffmpeg
 then
     notify-send -i "emblem-videos-symbolic" "Screencast" "Saved" 
@@ -23,16 +26,33 @@ else
         # Start recording. Codecs chosen according to compatibility with android
         # ffmpeg -y -f x11grab -s $RES -r 30 -i :0.0 -f alsa -i pulse -c:v h264 -c:a aac -ac 2 -vf scale=out_color_matrix=bt709 -pix_fmt yuv420p -movflags faststart $NAME
 
+        INT_MICROPHONE='alsa_input.pci-0000_00_1f.3.analog-stereo'
+        BT_HEADSET_SONY='bluez_sink.00_18_09_2E_74_B5.a2dp_sink.monitor'
+        BT_HEADSET_TT='bluez_sink.E8_07_BF_31_54_2C.a2dp_sink.monitor'
+        HDMI='alsa_output.pci-0000_00_1f.3.hdmi-stereo.monitor'
+
         ffmpeg -y \
-        -f x11grab -s $RES \
-        -i :0.0 \
-        -f alsa \
-        -i pulse \
-        -filter:a "volume=4.0" \
-        -c:v h264 -vf scale=out_color_matrix=bt709 -pix_fmt yuv420p \
-        -movflags faststart \
-        -r 30 \
-        -c:a aac -ac 2 $NAME
-        
+        -f x11grab -video_size 1920x1080 -grab_y 1080 -i :0.0 \
+        -f pulse -i $INT_MICROPHONE \
+        -f pulse -i $BT_HEADSET_TT \
+        -filter_complex \
+        "[1:a]volume=0.2[mic]; [2:a]volume=30.0[bt]; \
+        [mic][bt]amix=inputs=2[a]" \
+         -c:v h264 -vf scale=out_color_matrix=bt709 -pix_fmt yuv420p \
+         -movflags faststart \
+         -r 30 \
+        -map 0:v -map "[a]" -c:a aac $NAME
+
+        # ffmpeg -y \
+        # -f x11grab -s $RES \
+        # -i :0.0 \
+        # -f alsa \
+        # -i pulse \
+        # -filter:a "volume=4.0" \
+        # -c:v h264 -vf scale=out_color_matrix=bt709 -pix_fmt yuv420p \
+        # -movflags faststart \
+        # -r 30 \
+        # -c:a aac -ac 2 $NAME
+
     fi
 fi
