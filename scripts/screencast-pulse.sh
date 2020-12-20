8cb9356bbf8825a34ba068f4915ecc76f44f1fc2
lu0 <lucero.alvaradorz@gmail.com>
2020-12-20 02:50:50 -0600

    use current output sink in the screencast script

--- scripts/screencast-pulse.sh
+++ scripts/screencast-pulse.sh
@@ -4,7 +4,12 @@
 # lu0
 
 # list recording devices:
-# pacmd list-sources|awk '/index:/ {print $0}; /name:/ {print $0}; /device\.description/ {print $0}'
+# pacmd list-sources | awk '/index:/ {print $0}; /name:/ {print $0}; /device\.description/ {print $0}'
+# pacmd list-sources | awk '/index:/ {print $0}; /name:/ {print $0}; /device\.description/ {print $0}' | awk -F"[<>]" '{print $2}'
+#
+# list active output devices:
+# pacmd list-sink-inputs | awk '/index:/ {print $0}; /state:/ {print $0}; /sink:/ {print $0}'
+# pacmd list-sink-inputs | awk '/index:/ {print $0}; /state:/ {print $0}; /sink:/ {print $0}' | awk -F"[<>]" '{print $2}'
 
 if pgrep ffmpeg
 then
@@ -27,32 +32,24 @@ else
         # ffmpeg -y -f x11grab -s $RES -r 30 -i :0.0 -f alsa -i pulse -c:v h264 -c:a aac -ac 2 -vf scale=out_color_matrix=bt709 -pix_fmt yuv420p -movflags faststart $NAME
 
         INT_MICROPHONE='alsa_input.pci-0000_00_1f.3.analog-stereo'
-        BT_HEADSET_SONY='bluez_sink.00_18_09_2E_74_B5.a2dp_sink.monitor'
-        BT_HEADSET_TT='bluez_sink.E8_07_BF_31_54_2C.a2dp_sink.monitor'
-        HDMI='alsa_output.pci-0000_00_1f.3.hdmi-stereo.monitor'
+        # BT_HEADSET_SONY='bluez_sink.00_18_09_2E_74_B5.a2dp_sink.monitor'
+        # BT_HEADSET_TT='bluez_sink.E8_07_BF_31_54_2C.a2dp_sink.monitor'
+        # HDMI='alsa_output.pci-0000_00_1f.3.hdmi-stereo.monitor'
+
+        ACTIVE_OUTPUT=($(pacmd list-sink-inputs | awk '/index:/ {print $0}; /state:/ {print $0}; /sink:/ {print $0}' | awk -F"[<>]" '{print $2}'))
+        ACTIVE_OUTPUT_MONITOR="${ACTIVE_OUTPUT}.monitor"   # record monitor
 
         ffmpeg -y \
         -f x11grab -video_size 1920x1080 -grab_y 1080 -i :0.0 \
         -f pulse -i $INT_MICROPHONE \
-        -f pulse -i $BT_HEADSET_TT \
+        -f pulse -i $ACTIVE_OUTPUT_MONITOR \
         -filter_complex \
-        "[1:a]volume=0.2[mic]; [2:a]volume=30.0[bt]; \
-        [mic][bt]amix=inputs=2[a]" \
+        "[1:a]volume=0.3[mic]; [2:a]volume=60.0[output]; \
+        [mic][output]amix=inputs=2[a]" \
          -c:v h264 -vf scale=out_color_matrix=bt709 -pix_fmt yuv420p \
          -movflags faststart \
          -r 30 \
         -map 0:v -map "[a]" -c:a aac $NAME
-
-        # ffmpeg -y \
-        # -f x11grab -s $RES \
-        # -i :0.0 \
-        # -f alsa \
-        # -i pulse \
-        # -filter:a "volume=4.0" \
-        # -c:v h264 -vf scale=out_color_matrix=bt709 -pix_fmt yuv420p \
-        # -movflags faststart \
-        # -r 30 \
-        # -c:a aac -ac 2 $NAME
 
     fi
 fi
