6f8c88b417693ed724589d8f105def6431dd6f1a
lu0 <lucero.alvaradorz@gmail.com>
2020-12-27 02:00:18 -0600

    Update DPI script and mouse gestures

--- scripts/dpi.sh
+++ scripts/dpi.sh
@@ -9,23 +9,17 @@
 # xinput list-props 'Logitech M570'         # List properties
 # -------------------------------------------------------------
 
-CURRENT_PROFILE=$( xinput list-props 'Logitech M570' | grep 'libinput Accel Profile Enabled (' | cut -d ':' -f 2 | xargs )
+SLOW=-0.8
+FAST=-0.1
 CURRENT_SPEED=$( xinput list-props 'Logitech M570' | grep 'libinput Accel Speed (' | cut -d ':' -f 2 | xargs )
+CURRENT_PROFILE=$( xinput list-props 'Logitech M570' | grep 'libinput Accel Profile Enabled (' | cut -d ':' -f 2 | xargs )
 
 xinput --set-prop 'Logitech M570' 'libinput Accel Profile Enabled' 0, 1                                 # slowest profile
 
 [ "$CURRENT_PROFILE" != "0, 1" ] && CURRENT_SPEED=2                                                 # Force Default speed
 
-if (( $(echo "$CURRENT_SPEED == 1" | bc -l) )); then
-    xinput --set-prop 'Logitech M570' 'libinput Accel Speed' -0.8
-
-elif (( $(echo "$CURRENT_SPEED == -0.8" | bc -l ) )); then
-    xinput --set-prop 'Logitech M570' 'libinput Accel Speed' -0.1
-
-elif (( $(echo "$CURRENT_SPEED == -0.1" | bc -l ) )); then
-    xinput --set-prop 'Logitech M570' 'libinput Accel Speed'  1
-
+if (( $(echo "$CURRENT_SPEED == $FAST" | bc -l) )); then
+    xinput --set-prop 'Logitech M570' 'libinput Accel Speed' $SLOW
 else
-    xinput --set-prop 'Logitech M570' 'libinput Accel Speed'  -0.1
-    notify-send -i "input-mouse-symbolic" "Logitech M570" "Default DPI"
+    xinput --set-prop 'Logitech M570' 'libinput Accel Speed'  $FAST
 fi
