84287c480aa8058d5cee412bcb60fd95eda2cae4
lu0 <lucero.alvaradorz@gmail.com>
2021-08-01 15:49:20 -0500

    Refactor the trackpoint settings script to work with both built-in and external trackpoints.

--- scripts/trackpoint.sh
+++ scripts/trackpoint.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
-#--------------------------------------------------------------
-# Change mouse DPI
-# github.com/lu0
-# -------------------------------------------------------------
-# Bind it to a gesture   using
-# 'easystroke' (sudo apt-get install easystroke)
+
 #
-# xinput list-props 'TPPS/2 Elan TrackPoint'         # List properties
-# -------------------------------------------------------------
+# Toggle trackpoint speed
+#   github.com/lu0
+#
+# Bind it to a gesture using
+#   'easystroke' (sudo apt-get install easystroke)
+#
+# List devices:
+#   xinput list       # list devices
+#
+# List properties:
+#   xinput list-props 'TPPS/2 Elan TrackPoint'
+#   xinput list-props 'pointer:Lenovo ThinkPad Compact USB Keyboard with TrackPoint'
+#
 
-SLOW=-0.7
-FAST=0.1
-CURRENT_SPEED=$( xinput list-props 'TPPS/2 Elan TrackPoint' | grep 'libinput Accel Speed (' | cut -d ':' -f 2 | xargs )
-CURRENT_PROFILE=$( xinput list-props 'TPPS/2 Elan TrackPoint' | grep 'libinput Accel Profile Enabled (' | cut -d ':' -f 2 | xargs )
+function main() {
+    BUILT_IN_TRACKPOINT='TPPS/2 Elan TrackPoint'
+    toggle-trackpoint-speed "${BUILT_IN_TRACKPOINT}" -1 -0.6
+    
+    EXTERNAL_TRACKPOINT='pointer:Lenovo ThinkPad Compact USB Keyboard with TrackPoint'
+    toggle-trackpoint-speed "${EXTERNAL_TRACKPOINT}" -0.8 -0.5
+}
 
-xinput --set-prop 'TPPS/2 Elan TrackPoint' 'libinput Accel Profile Enabled' 1, 0
+function toggle-trackpoint-speed() {
+    DEVICE="${1}"
+    SLOW="${2}"
+    FAST="${3}"
 
-# xinput --set-prop 'TPPS/2 Elan TrackPoint' 'libinput Accel Profile Enabled' 0, 1
-# xinput --set-prop 'TPPS/2 Elan TrackPoint' 'libinput Accel Speed' 1
+    CURRENT_SPEED=$( xinput list-props "${DEVICE}" | grep 'libinput Accel Speed (' | cut -d ':' -f 2 | xargs )
+    CURRENT_PROFILE=$( xinput list-props "${DEVICE}" | grep 'libinput Accel Profile Enabled (' | cut -d ':' -f 2 | xargs )
 
-[ "$CURRENT_PROFILE" != "1, 0" ] && CURRENT_SPEED=2                                                 # Force Default speed
+    xinput --set-prop "${DEVICE}" 'libinput Accel Profile Enabled' 1, 0
 
-if (( $(echo "$CURRENT_SPEED == $FAST" | bc -l) )); then
-    xinput --set-prop 'TPPS/2 Elan TrackPoint' 'libinput Accel Speed' $SLOW
-else
-    xinput --set-prop 'TPPS/2 Elan TrackPoint' 'libinput Accel Speed'  $FAST
-fi
+    [ "$CURRENT_PROFILE" != "1, 0" ] && CURRENT_SPEED=2     # Force Default speed
+
+    (( $(echo "$CURRENT_SPEED == $FAST" | bc -l) )) \
+        && xinput --set-prop "${DEVICE}" 'libinput Accel Speed' $SLOW \
+        || xinput --set-prop "${DEVICE}" 'libinput Accel Speed'  $FAST
+}
+
+main
