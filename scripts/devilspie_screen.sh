d6ef2318db5bfa1971e9461e60faddfea3c54e9b
lu0 <me@lucerocodes.com>
2022-05-06 02:52:15 -0500

    Make devilspie custom script a general library: display_info

--- scripts/devilspie_screen.sh
+++ scripts/devilspie_screen.sh
@@ -4,51 +4,73 @@
 # This script gets information of the current monitor
 # 
 
-# Regex to match all contiguous numbers
-nums_re="[0-9]+"
+declare -A DISPLAY_INFO;                declare -a INFO_SORTING
+DISPLAY_INFO["monitor_name"]="";        INFO_SORTING+=("monitor_name")            
+DISPLAY_INFO["resolution"]="";          INFO_SORTING+=("resolution")        
+DISPLAY_INFO["x"]="";                   INFO_SORTING+=("x")
+DISPLAY_INFO["y"]="";                   INFO_SORTING+=("y")
+DISPLAY_INFO["width"]="";               INFO_SORTING+=("width")    
+DISPLAY_INFO["height"]="";              INFO_SORTING+=("height")    
+DISPLAY_INFO["window_id"]="";           INFO_SORTING+=("window_id")        
 
-# Regex to match lines [xres]x[yres]+[xoffset]+[yoffset]
-complete_re="^${nums_re}x${nums_re}\+${nums_re}\+${nums_re}$"
-#            │          │           │   match end of line ─┘
-#            │          │           └ + scaped (offset separator)
-#            │          └─ x (resolution separator)
-#            └─ match start of line
+# Show hashmap DISPLAY_INFO in the order specified by array INFO_SORTING
+display_info::show() {
+    for i in "${INFO_SORTING[@]}"; do
+        echo "${DISPLAY_INFO[${i}]}"
+    done
+}
 
-# Evaluates to variables X, Y, SCREEN, WINDOW
-eval "$(xdotool getmouselocation --shell)"
+# Fill hashmap DISPLAY_INFO
+display_info::store() {
+    # Regex to match all contiguous numbers
+    nums_re="[0-9]+"
 
-# shellcheck disable=2207 # Command unquoted to enable splitting
-monitor_names_array=( $(xrandr | grep -w connected | cut -d' ' -f1) )
+    # Regex to match lines [xres]x[yres]+[xoffset]+[yoffset]
+    complete_re="^${nums_re}x${nums_re}\+${nums_re}\+${nums_re}$"
+    #            │          │           │   match end of line ─┘
+    #            │          │           └ + scaped (offset separator)
+    #            │          └─ x (resolution separator)
+    #            └─ match start of line
 
-# Assumes sorting of `xrandr | grep -w connected` to be consistent (alphabetic)
-props_split_to_lines="$(xrandr | grep -w connected | awk '{OFS="\n"; $1=$1}1')"
-dimension_rows=$(echo -e "${props_split_to_lines}" | grep -Po "${complete_re}")
+    # Evaluates to variables X, Y, SCREEN, WINDOW
+    eval "$(xdotool getmouselocation --shell)"
+    DISPLAY_INFO[window_id]="${WINDOW}"
 
-monitor_index=0
-for dim in ${dimension_rows}; do
     # shellcheck disable=2207 # Command unquoted to enable splitting
-    dimension_cols_array=( $(echo "${dim}" | grep -Po "${nums_re}") )
-    res_x="${dimension_cols_array[0]}"
-    res_y="${dimension_cols_array[1]}"
-    off_x="${dimension_cols_array[2]}"
-    off_y="${dimension_cols_array[3]}"
+    monitor_names_array=( $(xrandr | grep -w connected | cut -d' ' -f1) )
 
-    if [ "${X}" -ge "${off_x}" ] && [ "${X}" -lt "$(( off_x + res_x ))" ] && \
-       [ "${Y}" -ge "${off_y}" ] && [ "${Y}" -lt "$(( off_y + res_y ))" ]
-    then
-        monitor_name="${monitor_names_array[monitor_index]}"
-        resolution="${res_x}x${res_y}"
-        break
-    fi
+    # Assumes sorting of `xrandr | grep -w connected` to be consistent (alphabetic)
+    props_split_to_lines="$(xrandr | grep -w connected | awk '{OFS="\n"; $1=$1}1')"
+    dimension_rows=$(echo -e "${props_split_to_lines}" | grep -Po "${complete_re}")
 
-    (( monitor_index++ ))
-done
+    monitor_index=0
+    for dim in ${dimension_rows}; do
+        # shellcheck disable=2207 # Command unquoted to enable splitting
+        dimension_cols_array=( $(echo "${dim}" | grep -Po "${nums_re}") )
+        res_x="${dimension_cols_array[0]}"
+        res_y="${dimension_cols_array[1]}"
+        off_x="${dimension_cols_array[2]}"
+        off_y="${dimension_cols_array[3]}"
 
-echo "Sending variables to lua..." >&2
-echo "$monitor_name"
-echo "$resolution"
-echo "$off_x"
-echo "$off_y"
-echo "$res_x"
-echo "$res_y"
-echo "$WINDOW"
+        if [ "${X}" -ge "${off_x}" ] && [ "${X}" -lt "$(( off_x + res_x ))" ] && \
+           [ "${Y}" -ge "${off_y}" ] && [ "${Y}" -lt "$(( off_y + res_y ))" ];
+        then
+            DISPLAY_INFO[monitor_name]="${monitor_names_array[monitor_index]}"
+            DISPLAY_INFO[resolution]="${res_x}x${res_y}"
+
+            DISPLAY_INFO[width]="${res_x}"
+            DISPLAY_INFO[height]="${res_y}"
+            DISPLAY_INFO[x]="${off_x}"
+            DISPLAY_INFO[y]="${off_y}"
+            break
+        fi
+
+        (( monitor_index++ ))
+    done
+}
+
+
+if [[ "${#BASH_SOURCE[@]}" -eq 1 ]]; then
+    display_info::store
+    display_info::show
+fi
