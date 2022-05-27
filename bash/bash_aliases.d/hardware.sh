1ec653d2f61638bda481701cc11d9f921db4b46b
lu0 <me@lucerocodes.com>
2022-05-26 23:33:15 -0500

    Refactor hardware aliases and make them shellcheck-compliant

--- bash/bash_aliases.d/hardware.sh
+++ bash/bash_aliases.d/hardware.sh
@@ -1,48 +1,96 @@
 #!/usr/bin/env bash
 
+# Manage battery on ThinkPads
 alias battery="sudo tlp-stat -b"
 alias fullcharge="sudo tlp fullcharge"
 
+# Redirect microphone to speaker
 alias mic2speaker-on="pactl load-module module-loopback latency_msec=1"
 alias mic2speaker-off="pactl unload-module module-loopback"
 
+# Reload mediakeys, sometimes they get confused with youtube/spotify
 alias mediakeys="killall csd-media-keys; csd-media-keys & disown"
+
+# Restart Cinnamon DE, useful when developing applets, themes, etc.
 alias cinnamon-restart="cinnamon --replace --display=:0"
 
-# Check shutdown status
-function shutdown-show() {
-    if [ -f /run/systemd/shutdown/scheduled ]; then
-        SHUTTIME=$(cat /run/systemd/shutdown/scheduled | grep "USEC" | cut -d '=' -f 2 | cut -c -10)
-        echo "Shutdown scheduled for $(date -d @$SHUTTIME), use 'shutdown -c' to cancel."
+# Check if automatic shutdown is set
+shutdown-show() {
+    local unix_timestamp human_timestamp
+    local file=/run/systemd/shutdown/scheduled
+    if [ -f "${file}" ]; then
+        unix_timestamp=$(grep "USEC" "${file}" | cut -d'=' -f2 | cut -c -10)
+        human_timestamp=$(date -d @"${unix_timestamp}")
+        echo "Shutdown scheduled for ${human_timestamp},"
+        echo "use 'shutdown -c' to cancel."
+    else
+        echo "Automatic shutdown is not set."
     fi
 }
 
-function wakeup() {
+# Manage automatic wakeup / boot
+wakeup() {
+    local alarm_file="/sys/class/rtc/rtc0/wakealarm"
+
+    _show_usage() {
+        echo -e "\nManage automatic wakeup (power-on/boot)."
+        echo -e "\nUSAGE:"
+        echo -e "   wakeup   [OPTIONS] ARGUMENTS"
+        echo -e "\nOPTIONS:"
+        echo -e "       -a      Show current wakeup alarm."
+        echo -e "       -c      Clear wakeup alarm."
+        echo -e "       -s      Set wakeup alarm:"
+        echo -e "                wakeup -s <time_HHmm> <plus_days>"
+        echo -e "                wakeup -s 2330 1 # tomorrow at 11:30pm\n"
+    }
+
+    _clear_alarm() {
+      sudo sh -c "echo 0 > ${alarm_file}"
+    }
+
+    _set_alarm() {
+        local wakeup_epoch="${1}"
+        sudo sh -c "echo ${wakeup_epoch} > ${alarm_file}"
+    }
+
+    _show_alarm() {
+        local unix_timestamp
+        unix_timestamp=$(cat ${alarm_file})
+        if [ "${unix_timestamp}" ]; then
+            local human_timestamp
+            human_timestamp=$(date -d @"${unix_timestamp}")
+            echo "Wakeup scheduled for ${human_timestamp},"
+            echo "use 'wakeup -c' to cancel."
+        else
+            echo "Automatic wakeup is not set."
+        fi
+    }
+
     OPTIND=1 # Reset getops
-    while getopts "acsd" opt; do
-        case $opt in
-        a)
-            ALARM=$(cat /sys/class/rtc/rtc0/wakealarm)
-            if [ $ALARM ]; then
-                echo "Wakeup scheduled for $(date -d @$ALARM), use 'wakeup -c' to cancel."
-            fi
-            ;;
-        c)
-            sudo sh -c "echo 0 > /sys/class/rtc/rtc0/wakealarm" # Clear alarm
-            echo -e "Wakeup canceled"
-            ;;
-        s)
-            HOUR=$2 # HHMM
-            DAY_DELAY=$3
+    while getopts acs opt; do
+        case "${opt}" in
+            a)
+                _show_alarm
+                ;;
+            c)
+                _clear_alarm
+                _show_alarm
+                ;;
+            s)
+                local wakeup_time_hhmm="${2:-0655}"
+                local delay_days="${3:-1}"
 
-            # Set date and time for wakeup alarm
-            DATE=$(date -d "+$DAY_DELAY day" +"%Y%m%d") # Tomorrow, YYYMMDD
-            WAKETIME=$(date -d "$DATE $HOUR" +%s)       # Convert to epoch
+                local wakeup_day wakeup_epoch
+                wakeup_day=$(date -d "+${delay_days} day" +"%Y%m%d")
+                wakeup_epoch=$(date -d "${wakeup_day} ${wakeup_time_hhmm}" +%s)
 
-            sudo sh -c "echo 0 > /sys/class/rtc/rtc0/wakealarm"         # Clear alarm
-            sudo sh -c "echo $WAKETIME > /sys/class/rtc/rtc0/wakealarm" # Set alarm
-            ;;
+                _clear_alarm
+                _set_alarm "${wakeup_epoch}"
+                _show_alarm
+                ;;
+            *)
+                _show_usage
+                ;;
         esac
     done
-    unset ALARM HOUR DAY_DELAY DATE WAKETIME
 }
