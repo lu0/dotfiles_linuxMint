d8195ffac1322f654f7c60cee7e1e7424709e6c7
lu0 <me@lucerocodes.com>
2022-05-29 00:16:07 -0500

    Add alias to retrieve numeric permissions of a blob.

--- bash/bash_aliases.d/filesystem.sh
+++ bash/bash_aliases.d/filesystem.sh
@@ -8,6 +8,8 @@ alias dusingle="sudo du -sh"            # size of directory
 alias duall="sudo du -hd 1 . | sort -h" # size of subdirectories (1st level)
 alias dfall="sudo df -h 1 .  | sort -h" # available space
 
+alias chmodcode="stat --format '%a'"
+
 # find symlinks in current pointing to given directory
 function findln() {
     find . -type l -exec readlink -nf {} ';' -exec echo " -> {}" ';' | grep "$*"
